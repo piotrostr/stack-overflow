95ec031e7b27f6b7210006af241f7ff66f2c8215
piotrostr <piotr.ostrowski@aol.com>
2022-05-29 17:03:18 +0200

    speed comparison

---
+++ speed-comparison-go-python/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	for i := 0; i < 1000000000; i++ {
+		// nothing
+	}
+	log.Println("Done")
+}
---
+++ speed-comparison-go-python/main.py
@@ -0,0 +1,3 @@
+for i in range(0, 1000000000):
+    5
+print("done")
